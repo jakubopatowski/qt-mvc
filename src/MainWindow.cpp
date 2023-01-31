f22e219e7afe313a5bf68d9def020e796660c293
Jakub Opatowski <jakubo@mikronika.com.pl>
2023-01-31 15:08:45 +0100

    Context menu for tableView

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,6 +2,14 @@
 
 MainWindow::MainWindow( QWidget* parent, Qt::WindowFlags flags ) : QMainWindow( parent, flags ) {
     ui.setupUi( this );
+
+    //setup context menu
+    ui.tableView->setContextMenuPolicy( Qt::CustomContextMenu );
+    tableMenu = new QMenu( ui.tableView );
+    tableMenu->addAction( ui.actionAdd );
+    tableMenu->addAction( ui.actionDelete );
+    connect( ui.tableView, &QTableView::customContextMenuRequested, this,
+             &MainWindow::slotCustomMenuRequested );
 }
 
 void MainWindow::setModel( std::shared_ptr< QAbstractTableModel >& model ) {
@@ -9,4 +17,9 @@ void MainWindow::setModel( std::shared_ptr< QAbstractTableModel >& model ) {
     m_sortFilter.setSourceModel( m_model.get() );
     ui.tableView->setModel( &m_sortFilter );
     ui.tableView->setSortingEnabled( true );
+    ui.tableView->setSelectionBehavior( QAbstractItemView::SelectionBehavior::SelectRows );
+}
+
+void MainWindow::slotCustomMenuRequested( QPoint pos ) {
+    tableMenu->popup( ui.tableView->viewport()->mapToGlobal( pos ) );
 }
--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -3,18 +3,22 @@
 #include "PersonalModel.hpp"
 #include "ui_MainWindow.h"
 #include <QMainWindow>
-#include <memory>
 #include <QSortFilterProxyModel>
+#include <memory>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
   public:
     explicit MainWindow( QWidget* parent = nullptr, Qt::WindowFlags flags = Qt::WindowFlags() );
-    void setModel( std::shared_ptr< QAbstractTableModel  >& model );
+    void setModel( std::shared_ptr< QAbstractTableModel >& model );
 
   private:
     Ui::MainWindow ui;
     std::shared_ptr< QAbstractTableModel > m_model;
     QSortFilterProxyModel m_sortFilter;
+    QMenu* tableMenu;
+
+  private slots:
+    void slotCustomMenuRequested( QPoint pos );
 };
